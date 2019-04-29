20bb8469f3400907284e8fc92363b80f058c9ea8
yangxingwu <yangxingwu@kuaishou.com>
2019-04-29 17:15:12 +0800

    add variables project

---
+++ variables/target/debug/deps/variables-18f2e403ae1ce6a7.d
@@ -0,0 +1,5 @@
+/home/yangxingwu/repos/The-Rust-Programming-Language/variables/target/debug/deps/variables-18f2e403ae1ce6a7: src/main.rs
+
+/home/yangxingwu/repos/The-Rust-Programming-Language/variables/target/debug/deps/variables-18f2e403ae1ce6a7.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/yangxingwu/repos/The-Rust-Programming-Language/variables/target/debug/variables: /home/yangxingwu/repos/The-Rust-Programming-Language/variables/src/main.rs
